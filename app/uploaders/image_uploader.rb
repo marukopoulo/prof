f1dcef5a4be3917e479f88db88a48b9ef814a025
marukopoulo <marukopoulo@gmail.com>
2015-04-26 19:04:43 +0900

    for android

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -45,7 +45,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
    def filename
-
+     
      "#{model.romaji}"
    end
 
